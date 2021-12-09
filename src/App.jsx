a33c22af9bd90a8bdc4d7677b6f9dccbf2a28b8b
Thomas Mueller <thomasmueller87@t-online.de>
2021-12-09 17:35:17 +0100

    API Data loaded

--- src/App.jsx
+++ src/App.jsx
@@ -14,21 +14,42 @@ function App() {
   //Reload der Seite nach dem Addbutton
   const [todos, setTodos] = useState([])
 
-  //Beim erstmaligen Laden der Seite die Todos aus dem LocalStorage holen
-  useEffect( function() {
-    loadFromLocal('NEWSAVE')
-  }, [])
-
-    function loadFromLocal(getdata) {
-      const todosFromLocal = JSON.parse(localStorage.getItem(getdata)) ?? []
-      setTodos(todosFromLocal)
-    }
-
-
-  // Wenn die Todos sich ändern, dann im LocalStorage speichern!
   useEffect(() => {
-    saveToLocal('NEWSAVE', todos)
-  }, [todos])
+    // 1. Hole Todos von der API
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      // 2. Wandle den Response in JSON um
+      .then((response) => response.json())
+      // 3. Verarbeite die Todos
+      .then((todosFromApi) => {
+        // 4. Erzeuge die Zielstruktur für die Daten
+        const allTodos = todosFromApi.slice(0, 9).map((todo) => ({
+          title: todo.title,
+          isDone: todo.completed,
+          color: 'gray',
+        }));
+
+        // Aktualisiere den State mit den Todos der API
+        setTodos(allTodos);
+      });
+  }, []); // Nur einmal beim App-Start
+
+
+  //PROGRAMM FÜR LOCAL STORAGE
+  //Beim erstmaligen Laden der Seite die Todos aus dem LocalStorage holen
+  // useEffect( function() {
+  //   loadFromLocal('NEWSAVE')
+  // }, [])
+
+  //   function loadFromLocal(getdata) {
+  //     const todosFromLocal = JSON.parse(localStorage.getItem(getdata)) ?? []
+  //     setTodos(todosFromLocal)
+  //   }
+
+
+  // // Wenn die Todos sich ändern, dann im LocalStorage speichern!
+  // useEffect(() => {
+  //   saveToLocal('NEWSAVE', todos)
+  // }, [todos])
 
     function saveToLocal(name, toSaveArray) {
       // localStorage.setItem(senddata, JSON.stringify(todos))
